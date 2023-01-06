8028199fabf59f0abf55efff99f0c0162990be8d
RaphD <111575644+RaphD31@users.noreply.github.com>
2023-01-06 14:55:33 +0100

    data insert

---
+++ sql/data-user.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (id, user_login, user_password) VALUES (1,'admin','$2a$10$E5HNQCq.miD/TvKAHqMIbuJKW8wXqlUWgLykopot8fySHd6.tAEGm');
+INSERT INTO users (id, user_login, user_password) VALUES (2,'guest','$2a$10$V5TV9Ox4q8N2RW4eiq3IQenDl6xEGPQ52kTGHuWcSw2hqly919MUm');
