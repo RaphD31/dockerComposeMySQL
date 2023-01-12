263d59c102ffc6bde51a5de2a6f55d7367a53af7
RaphD <111575644+RaphD31@users.noreply.github.com>
2023-01-12 15:36:48 +0100

    correctif MONTH in survey data

--- sql/data-survey.sql
+++ sql/data-survey.sql
@@ -1,7 +1,7 @@
 INSERT INTO survey(id, title, level, poe_type) 
-    VALUES (1 , 'Premier Retour', 'ONE_MOUNTH', 'POEI');
+    VALUES (1 , 'Premier Retour', 'ONE_MONTH', 'POEI');
 INSERT INTO survey(id, title, level, poe_type) 
-    VALUES (2 , 'deuxième retour', 'SIX_MOUNTH', 'POEI');
+    VALUES (2 , 'deuxième retour', 'SIX_MONTHS', 'POEI');
 INSERT INTO survey(id, title, level, poe_type) 
     VALUES (3 , 'Le Retour Final', 'ONE_YEAR', 'POEI');
     
