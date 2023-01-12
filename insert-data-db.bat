47ab914c03fd72ad81a378a8bb5ac5dafdb3533b
loicgardes <loic.gardes@gmail.com>
2023-01-12 15:01:47 +0100

    Create Answer Datas and Command Answer Datas Bat

--- insert-data-db.bat
+++ insert-data-db.bat
@@ -5,3 +5,4 @@ docker compose -p db-graal exec -T db mysql -u root -proot post_suivi_stagiaires
 docker compose -p db-graal exec -T db mysql -u root -proot post_suivi_stagiaires < sql\data-questions.sql
 docker compose -p db-graal exec -T db mysql -u root -proot post_suivi_stagiaires < sql\data-survey.sql
 docker compose -p db-graal exec -T db mysql -u root -proot post_suivi_stagiaires < sql\data-survey_contains_question.sql
+docker compose -p db-graal exec -T db mysql -u root -proot post_suivi_stagiaires < sql\data-answer.sql
